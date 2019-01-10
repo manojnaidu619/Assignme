ce042443e1805059983144d469e4b55a36e34a9a
Manoj Naidu <manojnaidu619@gmail.com>
2019-01-11 00:14:17 +0530

    1st level validation done

--- app/models/bio.rb
+++ app/models/bio.rb
@@ -2,4 +2,5 @@ class Bio < ApplicationRecord
   enum occupation: [:employed, :student, :freelancer, :other]
   enum gender: [:male, :female, :unspecified]
   belongs_to :user
+  #validates_presence_of :fullname,:username, :locality, :occupation, :contact_number, :college_school_name, :dob, :gender
 end
