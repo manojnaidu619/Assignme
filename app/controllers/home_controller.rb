f47368f134d0f7a8b1f724a25a84ec5b550e012d
Manoj Naidu <manojnaidu619@gmail.com>
2019-01-12 22:16:04 +0530

    Separate desk path for logged in users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    redirect_to desk_path if user_signed_in? or user_session
+  end
+
+  def desk
+    #code
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :bio, only:[:new, :create, :update, :edit]
   resources :works
+  get '/desk', to: 'home#desk'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
