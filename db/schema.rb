5c07a6c45edfeb92aa15a1c12865cc488e67a271
Manoj Naidu <manojnaidu619@gmail.com>
2019-01-15 18:35:09 +0530

    ad ends at

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_13_125936) do
+ActiveRecord::Schema.define(version: 2019_01_15_130412) do
 
   create_table "bios", force: :cascade do |t|
     t.string "fullname"
